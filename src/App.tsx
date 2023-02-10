233ded995ac8e48d21d6d23ceac3481a5385ff13
marcelli-lima <marcelli_lima@hotmail.com>
2023-02-10 10:07:25 -0300

    componente header, summary e tabela de transaçao

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { Transactions } from "./Pages/Transactions";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <Transactions />
+    </ThemeProvider>
+  );
+}
