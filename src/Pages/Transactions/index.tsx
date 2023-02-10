97a575d94d380da5f4ea322e9746ce82c99b2b6e
marcelli-lima <marcelli_lima@hotmail.com>
2023-02-10 10:11:57 -0300

    adiciona componente SearchForm

--- src/Pages/Transactions/index.tsx
+++ src/Pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../../components/Header";
+import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import {
   PriceHighlight,
@@ -12,6 +13,7 @@ export function Transactions() {
       <Header />
       <Summary />
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
